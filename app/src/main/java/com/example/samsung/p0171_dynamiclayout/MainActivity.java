9d7eee0ba6b387ae622048ec2d53ff14f510c6c0
btow <btow@yandex.ru>
2016-12-28 18:54:32 +0500

    Handler for button "Create" and "Clear" is created. It is complemented by a treatment of depression have dynamically generated buttons.

--- app/src/main/java/com/example/samsung/p0171_dynamiclayout/MainActivity.java
+++ app/src/main/java/com/example/samsung/p0171_dynamiclayout/MainActivity.java
@@ -2,12 +2,78 @@ package com.example.samsung.p0171_dynamiclayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    LinearLayout llMain;
+    RadioGroup rgGravity;
+    EditText etName;
+    Button btnCreate, btnClear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        llMain = (LinearLayout) findViewById(R.id.llMain);
+        rgGravity = (RadioGroup)findViewById(R.id.rgGravity);
+        etName = (EditText) findViewById(R.id.etName);
+        btnCreate = (Button) findViewById(R.id.btnCreate);
+        btnClear = (Button) findViewById(R.id.btnClear);
+
+        btnCreate.setOnClickListener(this);
+        btnClear.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        int viewId = v.getId();
+
+        switch (viewId) {
+
+            case R.id.btnCreate :
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT);
+
+                switch (rgGravity.getCheckedRadioButtonId()) {
+                    case R.id.rbLeft :
+                        layoutParams.gravity = Gravity.LEFT;
+                        break;
+                    case R.id.rbCenter :
+                        layoutParams.gravity = Gravity.CENTER_HORIZONTAL;
+                        break;
+                    case R.id.rbRight :
+                        layoutParams.gravity = Gravity.RIGHT;
+                        break;
+                }
+
+                Button btnNew = new Button(this);
+                btnNew.setId(llMain.getChildCount() + 1);
+                btnNew.setText(etName.getText().toString());
+                btnNew.setOnClickListener(this);
+                llMain.addView(btnNew, layoutParams);
+
+                break;
+            case R.id.btnClear :
+                llMain.removeAllViews();
+                Toast.makeText(MainActivity.this, "All views is cleared", Toast.LENGTH_SHORT).show();
+                break;
+            default :
+                Button button =  (Button) llMain.getChildAt(viewId);
+                button.setText(button.getText().toString() + " " + viewId);
+                break;
+        }
+
     }
 }
